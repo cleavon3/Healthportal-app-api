b3cdad5dc481197030f7bb97b59a4c85d446d80b
cleavon <cleavon.design.com>
2026-02-06 03:37:36 +0100

    configured docker compose and checks to wait for db

---
+++ app/app/calc.py
@@ -0,0 +1,14 @@
+"""
+Calculator function
+
+"""
+
+
+def add(x, y):
+    """Add x and y and return results."""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract x from y and return results."""
+    return y - x
---
+++ app/core/admin.py
@@ -0,0 +1,3 @@
+from django.db import models  # noqa
+
+# create your models here.
